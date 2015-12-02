595f947f69738e72a0476843556659d52326d8a9
Alena Y <ayampol1@binghamton.edu>
2015-12-01 23:22:20 -0500

    Added rudimentary moving between windows

---
+++ GUI_test.py
@@ -0,0 +1,85 @@
+#This code is based on http://pythonprogramming.net/change-show-new-frame-tkinter/
+
+import Tkinter as tk
+
+LARGE_FONT= ("Verdana", 12)
+
+class FrameWarden(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+        
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Welcome to the circus of values!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button = tk.Button(self, text="Info",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = tk.Button(self, text="Take a quiz?",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+        
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Info", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+         
+        
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Take a quiz!",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Quiz questions!! \n Or page two, actually.", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = tk.Button(self, text="Info",
+                            command=lambda: controller.show_frame(PageOne))
+        button2.pack()
+        
+app = FrameWarden()
+app.mainloop()
