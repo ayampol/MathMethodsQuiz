a663748af201fa7227bd08501debc0646801b6e1
Alena Y <ayampol1@binghamton.edu>
2015-12-10 18:23:46 -0500

    Forgot quizstate

---
+++ QuizState.py
@@ -0,0 +1,27 @@
+class QuizState(object):
+    def __init__(self):
+        self.type = type;
+        self.score = {'correct': 0, 'wrong': 0};
+        self.total_questions = 0;
+
+    def check_answer(self,choice):
+        self._update_question_total();
+        if (choice == 'correct'):
+            self._rightanswer();
+        else:
+            self._wronganswer();
+        
+    def _update_question_total(self):
+        self.total_questions = self.total_questions + 1;
+    
+    def _rightanswer(self):
+        self.score['correct'] = self.score['correct'] + 1;
+        
+    def _wronganswer(self):
+        self.score['wrong'] = self.score['wrong'] + 1;
+        
+    def get_result(self):
+        result = 'You got ' + self.score['correct'] + 'out of ' + self.total_questions + ' correct. '
+        return result;
+        
+            
